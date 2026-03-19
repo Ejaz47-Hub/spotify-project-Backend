d16f3c63f2f8cbda7160b07a2f48c57686a977e9
Ejaz47-Hub <ejaz47026@gmail.com>
2026-03-19 16:16:36 +0530

    updated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,8 +6,20 @@ import Musicrouter from "./routes/music.routes.js"
 const app = express()
 import cors from "cors";
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://spotify-project-backend.vercel.app",
+  "https://spotify-project-backend-ejaz47-hubs-projects.vercel.app"
+];
+
 app.use(cors({
-  origin: "https://spotify-project-backend.vercel.app", // ⚠️ replace with your real URL
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
 app.use(express.json({ limit: "50mb" }))
